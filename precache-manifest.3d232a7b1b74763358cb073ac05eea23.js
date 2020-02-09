43ff854a02694c1257d64c6189dfc88bb4e42c85
yatharth17 <yatharthkatyal4@gmail.com>
2020-02-09 07:18:49 +0530

    Updates

---
+++ precache-manifest.3d232a7b1b74763358cb073ac05eea23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42193664ae9ea7261ade268e362e5bc4",
+    "url": "/Monster-rolodex/index.html"
+  },
+  {
+    "revision": "7b50f3eaac857737660e",
+    "url": "/Monster-rolodex/static/css/main.0a925058.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/Monster-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Monster-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b50f3eaac857737660e",
+    "url": "/Monster-rolodex/static/js/main.a99037a5.chunk.js"
+  },
+  {
+    "revision": "887b87031a93572f86cc",
+    "url": "/Monster-rolodex/static/js/runtime-main.89826c57.js"
+  }
+]);
